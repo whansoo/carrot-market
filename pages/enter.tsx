d76186639f87c0ef161eadcaf05976dfbfe1b74f
whansoo <ghkstn7523@naver.com>
2023-03-19 21:16:05 +0900

    feat: Upload item page 구현

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -24,17 +24,17 @@ export default function Enter() {
           </div>
         </div>
         <form className="flex flex-col mt-8">
-          <label className="text-sm font-medium text-gray-700">
+          <label htmlFor="input" className="text-sm font-medium text-gray-700">
             {method === "email" ? "Email address" : null}
             {method === "phone" ? "Phone number" : null}
           </label>
           <div className="mt-1">
-            {method === "email" ? <input type="email" 
+            {method === "email" ? <input id="input" type="email" 
             className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500" required /> : null}
             {method === "phone" ? (
               <div className="flex rounded-md shadow-sm">
                 <span className="flex justify-center items-center px-3 border rounded-l-md border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm">+82</span>
-                <input type="number" className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500" required />
+                <input id="input" type="number" className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500" required />
               </div>
             ) : null}
           </div>
